230e1e7af4f01d3632c625fc533d7d78bd887ad3
dev-jayce <dev.jayce.kr@gmail.com>
2024-02-14 22:25:15 +0900

    Part 3. CORS 소개 및 관련 설정

--- src/main/java/com/fastcampus/board/config/WebConfiguration.java
+++ src/main/java/com/fastcampus/board/config/WebConfiguration.java
@@ -1,5 +1,7 @@
 package com.fastcampus.board.config;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,14 +9,31 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebConfiguration {
 
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(
+        Arrays.asList("http://localhost:3000", "http://127.0.0.1:3000"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/api/v1/**", configuration);
+    return source;
+  }
+
   // SpringBootWebSecurityConfiguration 커스터마이징
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+    http.cors(Customizer.withDefaults())
+        .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
         .sessionManagement(
             (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .csrf(CsrfConfigurer::disable)
